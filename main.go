539fc8afb7c9ace990c1acb6913a50ea1bbe094b
William <macwilliamdlc@gmail.com>
2022-07-16 12:54:59 -0500

    lesson 13: for loop

--- main.go
+++ main.go
@@ -105,4 +105,26 @@ func main() {
 	fmt.Println("doubleReturn:", value1, value2)
 	valuea, _ := doubleReturn(2)
 	fmt.Println("doubleReturn:", valuea)
+
+	// conditional for
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Printf("\n")
+
+	// for while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// for ever
+	//counterForever := 0
+	//for {
+	//	fmt.Println("Forever", counterForever)
+	//	counterForever++
+	//}
+
 }
