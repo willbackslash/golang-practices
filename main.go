61a84c8b19c6be3b5421637428eddd7e3601f4f7
William <macwilliamdlc@gmail.com>
2022-07-16 09:43:01 -0500

    class 7: variables

--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	// constants
+	const pi float64 = 3.14
+	const pi2 = 3.14
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2", pi2)
+
+	// variables int
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println(a, b, c, d)
+
+	// square area
+	const squareBase = 11
+	squareArea := squareBase * squareBase
+	fmt.Println("Area cuadrado:", squareArea)
 }
